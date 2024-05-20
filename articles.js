7a4a101e220dde530ffbb90fc2ae5f2b5597ffe6
Mohammad Reza Moradi <mohammadrezamoradi382@gmail.com>
2024-05-20 01:16:12 -0600

    first commit

---
+++ articles.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const articles = [
+        {
+            title: "Septimus Heap Book One: Magyk",
+            date: "July 5, 2022",
+            ageRange: "10-14",
+            genre: "Fantasy",
+            rating: "****",
+            description: "If you enjoy stories about seventh sons of seventh sons and magyk, this is the book for you.",
+            image: "path/to/magyk.jpg"
+        },
+        {
+            title: "Magnus Chase Book One: Sword of Summer",
+            date: "December 12, 2021",
+            ageRange: "12-16",
+            genre: "Fantasy",
+            rating: "*****",
+            description: "Magnus Chase, a homeless boy living in Boston, finds out he is the son of a Norse god.",
+            image: "path/to/sword_of_summer.jpg"
+        }
+    ];
